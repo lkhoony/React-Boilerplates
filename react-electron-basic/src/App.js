4e216563b22c7f06ed0dd3e6e0a2c01234a424a9
lkhoony <lkh14011424@gmail.com>
2021-12-13 10:37:20 +0900

    react-electron-basic 설정파일, HMR 업데이트

--- react-electron-basic/src/App.js
+++ react-electron-basic/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { hot } from 'react-hot-loader/root'
+import { hot } from 'react-hot-loader'
 
 const App = () =>{
     return(
         <div>hello, webpack!</div>
     )
 }
-export default hot(App);
+export default hot(module)(App);
